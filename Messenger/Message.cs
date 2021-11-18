caa5fa69078ea51d325bf9db93d22961c101bf49
Zeker10n <tristan.edward.hoenninger@gmail.com>
2021-11-18 00:37:27 -0500

    Updating KeyGen and sendingMessage

--- Messenger/Message.cs
+++ Messenger/Message.cs
@@ -16,9 +16,9 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
-            Task task = Messenger.getKey("jsb@cs.rit.edu");
-            task.Wait();
-            Messenger.encryptMessage("jsb@cs.rit.edu");
+            //Task task = Messenger.getKey("jsb@cs.rit.edu");
+            //task.Wait();
+            
         }
     }
 
@@ -48,12 +48,51 @@ namespace Messenger
             var curDirPath = Directory.GetCurrentDirectory();
             var publicPath = curDirPath + "\\public.key";
             var privatePath = curDirPath + "\\private.key";
+            PrimeGen prime = new PrimeGen();
             BigInteger E = 65537;
-            
-            
+            BigInteger p = prime.SetupGen(512);
+            BigInteger q = prime.SetupGen(512);
+            BigInteger N = p * q;
+            BigInteger r = (p - 1) * (q - 1);
+            BigInteger D = modInverse(E, r);
+            var DBytes = genBytes(D, true);
+            var EBytes = genBytes(E, true);
+            var NBytes = genBytes(N, true);
+            var d = BitConverter.GetBytes(DBytes.Length);
+            var e = BitConverter.GetBytes(EBytes.Length);
+            var n = BitConverter.GetBytes(NBytes.Length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(d);
+                Array.Reverse(e);
+                Array.Reverse(n);
+            }
+            Console.WriteLine(d.Length);
         }
 
-        public static void encryptMessage(String email)
+        private byte[] genBytes(BigInteger num, bool littleEndian)
+        {
+            var big = num.ToByteArray();
+            
+            if (BitConverter.IsLittleEndian)
+            {
+                if (!littleEndian)
+                {
+                    big = big.Reverse().ToArray();
+                }
+            }
+            else
+            {
+                if (littleEndian)
+                {
+                    big = big.Reverse().ToArray();
+                }
+            }
+            
+            return big;
+        }
+
+        public static async Task sendMsg(String email, String Msg)
         {
             var curDirPath = Directory.GetCurrentDirectory();
             var emailPath = curDirPath + '\\' + email + ".key";
@@ -61,26 +100,33 @@ namespace Messenger
             {
                 String json = File.ReadAllText(emailPath);
                 var publicKey = JsonConvert.DeserializeObject<PublicKey>(json);
-                var encoded = publicKey.key;
-                byte[] arr = Convert.FromBase64String(encoded);
-                var size = new byte[4];
-                Array.Copy(arr, size, 4);
-                size = size.Reverse().ToArray();
-                var e = BitConverter.ToInt32(size);
-                var tempArr = new byte[e];
-                Array.Copy(arr, 4, tempArr, 0, e);
-                var E = new BigInteger(tempArr);
-                Array.Copy(arr, 4 + e, size, 0, 4);
-                size = size.Reverse().ToArray();
-                var n = BitConverter.ToInt32(size);
-                tempArr = new byte[n];
-                Array.Copy(arr, 4 + e + 4, tempArr, 0, n);
-                var N = new BigInteger(tempArr);
+                var encodedKey = publicKey.key;
+                
             }
             else
             {
                 
             }
+        }
+
+        private BigInteger changeMessage(String key, BigInteger message)
+        {
+            byte[] arr = Convert.FromBase64String(key);
+            var size = new byte[4];
+            Array.Copy(arr, size, 4);
+            size = size.Reverse().ToArray();
+            var eOrd = BitConverter.ToInt32(size);
+            var tempArr = new byte[eOrd];
+            Array.Copy(arr, 4, tempArr, 0, eOrd);
+            var EOrD = new BigInteger(tempArr);
+            Array.Copy(arr, 4 + eOrd, size, 0, 4);
+            size = size.Reverse().ToArray();
+            var n = BitConverter.ToInt32(size);
+            tempArr = new byte[n];
+            Array.Copy(arr, 4 + eOrd + 4, tempArr, 0, n);
+            var N = new BigInteger(tempArr);
+            var changedMsg = BigInteger.ModPow(message, EOrD, N);
+            return changedMsg;
         }
         
         public static async Task getKey(string email)
